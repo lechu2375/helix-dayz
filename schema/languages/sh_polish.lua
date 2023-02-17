f85fc3e782d068e2b49ccd08ce1c085ecf306c4a
Lechu2375 <adamos2375@gmail.com>
2023-02-17 13:23:11 +0100

    spolszczenie pliku

---
+++ schema/languages/sh_polish.lua
@@ -0,0 +1,183 @@
+LANGUAGE = {
+	optDrawSafezones = "Wyłącz pokazywanie bezpiecznych stref.",
+	optdDrawSafezones = "Wyłącza pokazywanie bezpiecznych stref np. zielone kule.",
+
+	optEntityShadows = "Cienie entity",
+	optdEntityShadows = "Przełącza pomiędzy wyświetlaniem cieni dla entity.",
+
+	optDrawOutlineSZ = "Wyłącza pokazywanie graczy w bezpiecznych strefach.",
+
+	optColorSafezones = "Kolor bezpiecznej strefy.",
+	optColorSafezonePlayers = "Kolor gracza w bezpiecznej strefie.",
+	optColorOutlineItems = "Kolor obrysu dla przedmiotów.",
+	optdColorOutlineItems = "Kiedy patrzysz na przedmiot.",
+	optSquadTeamColor = "Kolor drużyny",
+	optdSquadTeamColor = "Na mapie, czacie, etc.",
+
+	optToggleEffects = "Efekty pierwszoosobowe",
+
+	colors = "Kolory",
+
+	option_remove = "Usuń",
+	option_edit = "Edytuj",
+	option_done = "Gotowe",
+	option_use = "Użyj",
+
+	Health = "Zdrowie",
+	text_yes = "Tak",
+	text_no = "Nie",
+
+	global_ooc_disabled = "Globalny czat OOC jest wyłączony.",
+
+	// primary_needs
+	well_fed = "Najedzony",
+	needs_hunger = "Głód",
+	needs_thirst = "Pragnienie",
+
+	// stamina
+	Stamina = "Wytrzymałość",
+
+	// radiation
+	Radiation = "Napromieniowanie",
+
+	// enchanced_damage
+	broken_leg = "ZŁAMANA NOGA",
+	bleeding_type_mild = "ŁAGODNY",
+	bleeding_type_average = "UMIARKOWANY",
+	bleeding_type_serious = "POWAŻNY",
+	bleeding_type_heavy = "CIĘŻKI",
+	bleeding_blood_loss = "UTRATA KRWI",
+
+	// safezone
+	safezone_title = "Bezpieczna strefa",
+
+	// Reputation
+	reputation = "Reputacja",
+	combat_logged = "COMBAT LOGGED",
+	cannot_enter_safezone = "NIE MOŻNA OPUŚCIĆ BEZPIECZNEJ STREFY",
+
+	// chat_channels
+	tradeChatDelay = "Musisz poczekać jeszcze %s sekund przed następnym handlem!",
+	tradeChatPrefix = "[Handel] ",
+	globalChatPrefix = "[Global] ",
+	localChatPrefix = "[Local] ",
+	squadChatPrefix = "[Drużyna] ",
+	onlyEnglishLetters = "Only english is allowed in global chat.",
+	cmdTradeChat = "Wysyła wiadomość w czacie handlu.",
+	cmdSquadChat = "Wysyła wiadomość do drużyny.",
+
+	cmdOOC = "Wysyła globalną wiadomość OOC.",
+
+	// Durability
+	Repair = "Napraw",
+	RepairKitWrong = 'Nie masz zestawu naprawczego!',
+	DurabilityUnusableTip = "Ta broń jest zepsuta.",
+	conditionDesc = "Wytrzymałość: %s (%s%%)",
+	txtCond0 = "Nowa",
+	txtCond1 = "Prawie nowa",
+	txtCond2 = "Widoczne ślady użycia",
+	txtCond3 = "Ledwo działa",
+	txtCond4 = "Popsuta",
+	weaponJammed = "Twoja broń się zacięła! Przeładuj ją!",
+	raiseDurability = "Wytrzymałość",
+
+	// Grave
+	here_lies = "Tu leży",
+	to_loot = "by przeszukać",
+	grave = "Grób",
+
+	// Merchant
+	merchant_title = "Sprzedawca",
+
+	// Stash
+	stash_title = "Schowek",
+	stash_title_count = "Schowek %d/%d",
+	stash_desc = "Schowaj tu swoje rzeczy i pieniądze",
+	stash_far = "Za daleko od schowka.",
+	stash_full = "Ten przedmiot się tu nie zmieści!",
+	stash_enter_money = "Wpisz liczbę",
+
+	derma_request_cancel = "Anuluj",
+
+	deposit = "Zdeponuj",
+	withdraw = "Wyjmij",
+
+	// ArcCW
+	["Empty clip"] = "Pusty magazynek",
+	arccw_alreadyAttached = "Ten slot jest zajęty!",
+
+	Consume = "Skonsumuj",
+	Attach = "Przyczep",
+	Split = "Rozdziel",
+
+	split_enter_quantity = "Wpisz liczbę",
+
+	// squad
+	squad_menu_title = "MENU DRUŻYNY",
+	squad_menu_messageday = "Message of the Day",
+	squad_menu_holdername = "Wpisz nazwę drużyny",
+	squad_menu_holderdesc = "Wpisz wiadomość dnia",
+	squad_menu_opt_sendpm = "Wyślij PW",
+	squad_menu_opt_message = "Wiadomość",
+	squad_menu_btn_raiseofficer = "Awansuj do rangi oficera",
+	squad_menu_btn_ask_raiseofficer = "Na pewno?",
+	squad_menu_btn_raise = "Awansuj",
+	squad_menu_text_Leader = "Lider",
+	squad_menu_text_Officers = "Oficerowie",
+	squad_menu_text_Members = "Członkowie",
+	squad_menu_text_logo = "Logo",
+	squad_menu_text_logo2 = "Logo drużyny",
+	squad_menu_holderlogo = "Link do loga (tylko www.imgur.com)",
+	squad_menu_btn_invite = "Zaproś",
+	squad_menu_btn_color = "Zmień kolor drużyny",
+	squad_menu_btn_disband = "Rozwiąż drużynę",
+	squad_menu_btn_ask_disband = "Na pewno chcesz rozwiązać nieodwracalnie drużynę?",
+	squad_menu_btn_leave = "Opuść drużynę",
+	squad_menu_btn_editdesc = "Edytuj (maksymalna ilość znaków: 2048)",
+	squad_menu_btn_save = "Zapisz",
+	squad_menu_btn_demote = "Zdegraduj do członka",
+	squad_menu_btn_ask_demote = "Na pewno?",
+	squad_menu_text_demote = "Zdegraduj",
+	squad_menu_btn_kickmember = "Wyrzuć członka",
+	squad_menu_text_kickmember = "Na pewno chcesz wyrzucić %s z %s?",
+	squad_leader_disbanded = "Lider rozwiązał drużynę!",
+	squad_kicked = "Zostałeś wyrzucony z drużyny!",
+	squad_successfully_left = "Opuściłeś drużynę.",
+	squad_create_wait = "Twoja drużyna jest tworzona...",
+	squad_promoted_rank = "Zostałeś awansowany do rangi oficera.",
+	squad_promoted_rank2 = "Członek drużyny %s został awansowany do rangi oficera.",
+	squad_demoted_rank = "Zostałeś zdegradowany do rangi oficera.",
+	squad_demoted_rank2 = "Członek drużyny %s został zdegradowany.",
+	squad_successfully_created = "Drużyna %s została pomyślnie stworzona!",
+	squad_invite = "%s zaprasza Cię do drużyny %s. Wpisz /saccept by akceptować zaproszenie",
+	squad_invite_accepted = "%s zaakceptował Twoje zaproszenie do drużyny.",
+	squad_invite_accepted2 = "Zostałeś dodany do drużyny: %s",
+	
+
+	// Flash window
+	optFlashWindow = "Flash Window",
+	optdFlashWindow = "Whether the window should blink when the game is minimized.",
+
+	// Inventory
+	text_rmb_takeallmoney = "RMB - zabierz całą kasę",
+	text_rmb_giveallmoney = "RMB - daj całą kasę",
+	text_rmb_dropallmoney = "RMB - wyrzuć całą kasę",
+	take_all = "Zbierz wszystko",
+
+	// Player spawn points
+	psp_btn_spawnatsafe = "Spawn w Bezpiecznej Strefie",
+	psp_btn_spawnrandomly = "Spawn losowy",
+	psp_btn_disconnect = "Rozłącz się",
+
+	// clothes
+	clothes_text_slot = "Slot: %s",
+
+	// Scoreboard
+	scoreboard_players = "GRACZE:",
+
+	// killfeed
+	["killfeed.death.bledout"] = "wykrwawił się",
+	["killfeed.death.radiation"] = "choroba radiacyjna",
+	["killfeed.death.drowned"] = "utonął",
+	["killfeed.death.overdose"] = "przedawkował",
+}
