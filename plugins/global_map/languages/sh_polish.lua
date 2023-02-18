cbdeef9c59e7e006ebc6964e078665bc560814fd
Lechu2375 <adamos2375@gmail.com>
2023-02-18 13:20:18 +0100

    Create sh_polish.lua

---
+++ plugins/global_map/languages/sh_polish.lua
@@ -0,0 +1,12 @@
+LANGUAGE = {
+	globalMapDeleteLabelsConfirm = "Chcesz usunąć wszystkie znaczniki?",
+	globalMapDeleteLabels = "Usuń wszystkie znaczniki",
+	globalMapLabel = "Znacznik",
+	globalMapPlaceholderLabel = "Wpisz nazwę znacznika",
+	globalMapLabelName = "Nazwa znacznika",
+	globalMap = "Mapa",
+	globalMapYOU = "TY",
+	globalMapCursor = "Wciśnij %s by %s kursor myszki",
+	globalMapCursorEnable = "włączyć",
+	globalMapCursorDisable = "wyłączyć"
+}
