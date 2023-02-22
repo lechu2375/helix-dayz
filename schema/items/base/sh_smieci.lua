2238b1429ea1e584c6e6cb7d30ba9657553744db
rewelacja <mikolajzbik11@gmail.com>
2023-02-22 18:28:44 +0100

    Śmieci
    Próbuje zrobić kategorię w folderze items oraz testowy przedmiot.

---
+++ schema/items/base/sh_smieci.lua
@@ -0,0 +1,6 @@
+ITEM.base = "base_smieci"
+ITEM.name = "Armored Clothes"
+ITEM.description = "A suitcase full of clothes."
+ITEM.model = Model("models/props_c17/suitcase_passenger_physics.mdl")
+ITEM.category = "Armored Clothing"
+ITEM.width = 2
---
+++ schema/items/smieci/sh_weaponparts_lua.lua
@@ -0,0 +1,4 @@
+ITEM.name = "Części do broni"
+ITEM.model = Model("models/illusion/eftcontainers/weaponparts.mdl")
+ITEM.description = "Sterta rozmaitych części do broni, przydatna do naprawiania rynsztunku oraz wytwarzania nowego."
+ITEM.category = "Użyteczne"
