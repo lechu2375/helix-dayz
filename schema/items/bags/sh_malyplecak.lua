44523c026e07a07e0fa56691ca45eefa5822a426
rewelacja <mikolajzbik11@gmail.com>
2023-02-26 19:09:26 +0100

    Pluginy i plecaki
    Zrobienie dwóch przedmiotów przechowawczych.
    
    Usunięcie serverguardsupport bo mamy sam menu.
    
    Dodanie pluginów
    - Permaremove.
    - Playerconnected.

---
+++ schema/items/bags/sh_malyplecak.lua
@@ -0,0 +1,6 @@
+ITEM.name = "Mały plecak"
+ITEM.description = "Mały plecak wojskowy, idealny do przechowywania przedmiotów. Jest nieprzemakalny i trwały, doskonały na dłuższe podróże."
+ITEM.model = Model("models/tankbags/small_bags/smallbag05.mdl")
+ITEM.category = "Użytkowe"
+ITEM.invWidth = 4
+ITEM.invHeight = 6
---
+++ schema/items/bags/sh_plecak.lua
@@ -0,0 +1,6 @@
+ITEM.name = "Plecak"
+ITEM.description = "Plecak wojskowy, idealny do przechowywania przedmiotów. Jest nieprzemakalny i trwały, doskonały na dłuższe podróże chodź nieporęczny."
+ITEM.model = Model("models/tankbags/small_bags/smallbag02.mdl")
+ITEM.category = "Użytkowe"
+ITEM.invWidth = 6
+ITEM.invHeight = 6
