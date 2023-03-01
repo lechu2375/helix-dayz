1c920e3d74931d5524ef12c933e92cbf97124340
Lechu2375 <adamos2375@gmail.com>
2023-03-01 10:53:38 +0100

    naprawienie pluginu na latarę

--- plugins/flashlightitem/sh_plugin.lua
+++ plugins/flashlightitem/sh_plugin.lua
@@ -4,10 +4,13 @@ PLUGIN.author = "SleepyMode"
 PLUGIN.description = "Adds an item allowing players to toggle their flashlight."
 
 function PLUGIN:PlayerSwitchFlashlight(client, bEnabled)
+	
 	local character = client:GetCharacter()
 	local inventory = character and character:GetInventory()
 
 	if (inventory and inventory:HasItem("flashlight")) then
 		return true
+	else
+		return false 
 	end
 end
