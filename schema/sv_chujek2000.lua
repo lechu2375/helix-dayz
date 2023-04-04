3484c0ae6e0c39dfcb3b904d68dfceb7b2f8ab8e
Lechu2375 <adamos2375@gmail.com>
2023-04-04 13:42:37 +0200

    Update sv_chujek2000.lua

--- schema/sv_chujek2000.lua
+++ schema/sv_chujek2000.lua
@@ -2,7 +2,7 @@ local ACTable = ix.data.Get("antyzjeb", {}, true, true, true)
 
 function Schema:PlayerInitialSpawn(ply)
 
-    if(!ACTable[ply:SteamID64()]) then
+    if(!ACTable[ply:SteamID64()]) then //no table for player then create new
         ACTable[ply:SteamID64()] = {}
         ACTable[ply:SteamID64()].hits = {}
     end
@@ -11,6 +11,33 @@ end
 function Schema:ScalePlayerDamage( poszkodowany,hitgroup,dmginfo )
     local attacker = dmginfo:GetInflictor()
     local actable = ACTable[attacker:SteamID64()]
-    actable.hits[hitgroup] = (actable.hits[hitgroup] or 0) + 1
+    actable.hits[hitgroup] = (actable.hits[hitgroup] or 0) + 1 
 end
 
+
+local meta = FindMetaTable("Player")
+
+
+function meta:GetAllHitsCount(hitgroup)
+    local actable = ACTable[self:SteamID64()]
+    local count = 0
+    if(hitgroup) then
+        return actable.hits[hitgroup]
+    else
+        for _,v in pairs(actable.hits) do 
+            count = count + v
+        end
+        return count
+    end 
+end
+
+function meta:GetHitPercByHitgroup(hitgroup)
+    local actable = ACTable[self:SteamID64()]
+    if(actable.hits[hitgroup]) then
+        local hitCount = self:GetAllHitsCount(false)
+        local hitgroupCount = self:GetAllHitsCount(hitgroup)
+        local percentage = hitgroupCount/hitCount
+        return percentage
+    end
+    return false //wrong hitgroup
+end
