7cc5aa4a5a97da7d0bcf389a73f26b4e0bd83104
Lechu2375 <adamos2375@gmail.com>
2023-04-07 19:32:59 +0200

    Update cl_schema.lua

--- schema/cl_schema.lua
+++ schema/cl_schema.lua
@@ -52,6 +52,13 @@ local ver = "DayZ Rozdział 1 wer. 0.70704a"
 local w = draw.GetFontHeight( "DebugFixedSmall")
 surface.SetFont("DebugFixedSmall")
 local h = surface.GetTextSize(ver)
+local wave = Material( "dayzlogo.png", "noclamp smooth" )
 function Schema:PostRenderVGUI()
 	ix.util.DrawText(ver, ScrW()-h, ScrH()-w, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT, "DebugFixedSmall", 255)
+	surface.SetMaterial( wave )
+	surface.SetDrawColor( 255, 255, 255, 50 )
+	surface.DrawTexturedRect( 0, 0, 128, 112 )
 end
+
+
+
