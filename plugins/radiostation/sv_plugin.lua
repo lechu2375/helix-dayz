a67c3579076ad74ef58b3cace47067087c80a57f
Lechu2375 <adamos2375@gmail.com>
2023-04-12 18:15:54 +0200

    respienie mikro

--- plugins/radiostation/sv_plugin.lua
+++ plugins/radiostation/sv_plugin.lua
@@ -18,6 +18,23 @@ function PLUGIN:LoadRadiostations()
 		radiostation:Spawn()
         radiostation:GetPhysicsObject():EnableMotion(false) //freezee!!
 	end
+
+    local mikropfone = ents.Create("radio_microphone")
+    mikropfone:SetPos(Vector("1568.612061 5181.766113 912.031250"))
+    //radiostation:SetAngles(angle_zero)
+    mikropfone:Spawn()
+    mikropfone:GetPhysicsObject():EnableMotion(false) //freezee!!
+    //i huj jazda niech odpierdalają z mikro
+
+    if(!timer.Exists("radioStationsEnabler")) then
+        timer.Create("radioStationsEnabler", 120, 0, function() //nie ma kurwy jebane macie sluchac pierdolenia az kurwicy dostaniecie
+            for _, v in ipairs(ents.FindByClass("radio_receiver")) do
+                if(!v.dt.On) then 
+                    v.dt.On = true
+                end
+            end       
+        end)
+    end
 end
 
 function PLUGIN:LoadData()
