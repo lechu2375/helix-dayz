7fd092303578841d7d0f3b1f6320f3af1fe0bc6e
Lechu2375 <adamos2375@gmail.com>
2023-05-27 09:42:03 +0200

    Update sv_core.lua

--- plugins/bagsenchancer/sv_core.lua
+++ plugins/bagsenchancer/sv_core.lua
@@ -1,11 +1,19 @@
 local CHAR = ix.meta.character or
 
-function CHAR:FindAllBags()
+function CHAR:FindAllBagsInInventory()
     local foundTable = {}
     for k,v in pairs(self:GetInventory():GetItems(true)) do
-        if(v.invWidth) then
-
+        if(v.invWidth) then //v.pacmodel
+            foundTable[#foundTable+1] = v
         end
     end
-    return foundTable
+        return foundTable
 end
+
+concommand.Add("testcmd", function(ply)
+
+local char = ply:GetCharacter()
+    print("found:")
+    PrintTable(char:FindAllBagsInInventory())
+
+end)
