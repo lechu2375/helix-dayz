2df98f6019e577225f7cae902cdbcde332ceebfc
Lechu2375 <adamos2375@gmail.com>
2023-05-27 13:20:37 +0200

    jebacneplecaki

--- schema/items/bags/sh_blackjack.lua
+++ schema/items/bags/sh_blackjack.lua
@@ -6,3 +6,88 @@ ITEM.category = "Użytkowe"
 ITEM.price = 3500
 ITEM.invWidth = 5
 ITEM.invHeight = 7
+ITEM.pacData =  {
+	["children"] = {
+		[1] = {
+			["children"] = {
+			},
+			["self"] = {
+				["Skin"] = 0,
+				["UniqueID"] = "e55790b048b1170dad4ada52abac950dc81675516e6041f9a9d5ab7d74459ac5",
+				["NoLighting"] = false,
+				["AimPartName"] = "",
+				["IgnoreZ"] = false,
+				["AimPartUID"] = "",
+				["Materials"] = "",
+				["Name"] = "",
+				["LevelOfDetail"] = 0,
+				["NoTextureFiltering"] = false,
+				["PositionOffset"] = Vector(0, 0, 0),
+				["IsDisturbing"] = false,
+				["EyeAngles"] = false,
+				["DrawOrder"] = 0,
+				["TargetEntityUID"] = "",
+				["Alpha"] = 1,
+				["Material"] = "",
+				["Invert"] = false,
+				["ForceObjUrl"] = false,
+				["Bone"] = "head",
+				["Angles"] = Angle(0, 0, 0),
+				["AngleOffset"] = Angle(0, 0, 0),
+				["BoneMerge"] = true,
+				["Color"] = Vector(1, 1, 1),
+				["Position"] = Vector(0, 0, 0),
+				["ClassName"] = "model2",
+				["Brightness"] = 1,
+				["Hide"] = false,
+				["NoCulling"] = false,
+				["Scale"] = Vector(1, 1, 1),
+				["LegacyTransform"] = false,
+				["EditorExpand"] = false,
+				["Size"] = 1,
+				["ModelModifiers"] = "",
+				["Translucent"] = false,
+				["BlendMode"] = "",
+				["EyeTargetUID"] = "",
+				["Model"] = "models/player/backpack_baselardwild/bp_load_baselard_body_lod0.mdl",
+			},
+		},
+	},
+	["self"] = {
+		["DrawOrder"] = 0,
+		["UniqueID"] = "d205fbec34802bd3b4fb840198d94a859790ad53627dc45f1225b6dc7cb6c223",
+		["Hide"] = false,
+		["TargetEntityUID"] = "",
+		["EditorExpand"] = true,
+		["OwnerName"] = "self",
+		["IsDisturbing"] = false,
+		["Name"] = "",
+		["Duplicate"] = false,
+		["ClassName"] = "group",
+	},
+}
+
+
+
+
+ITEM.functions.Equip = {
+	name = "Equip",
+	tip = "equipTip",
+	icon = "icon16/tick.png",
+	OnRun = function(item)
+        item.player:AddPart("plecak", item)
+		return false
+	end,
+
+}
+
+ITEM.functions.UnEquip = {
+	name = "UnEquip",
+	tip = "unequipTip",
+	icon = "icon16/tick.png",
+	OnRun = function(item)
+        item.player:RemovePart("plecak")
+		return false
+	end,
+
+}
