6e9044460b8501644aea2510966c5ed3d04c78e7
Lechu2375 <adamos2375@gmail.com>
2023-06-08 20:52:54 +0200

    naprawa ammo do szogunów

--- plugins/arccw_support/items/arccw_ammo/sh_12gauge.lua
+++ plugins/arccw_support/items/arccw_ammo/sh_12gauge.lua
@@ -1,6 +1,6 @@
 ITEM.name = "Amunicja 12"
 ITEM.model = "models/gmodz/ammo/12x70.mdl"
-ITEM.ammo = "12 Gauge"
+ITEM.ammo = "12gauge"
 ITEM.ammoAmount = 16
 ITEM.maxRounds = 32
 ITEM.description =  "Paczka amunicji zawierająca kaliber 12"
