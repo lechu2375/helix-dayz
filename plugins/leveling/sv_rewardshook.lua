e8c00f609641f803ea0f91a823c6bb9188c424d5
Lechu2375 <adamos2375@gmail.com>
2023-06-17 11:11:32 +0200

    Update sv_rewardshook.lua

--- plugins/leveling/sv_rewardshook.lua
+++ plugins/leveling/sv_rewardshook.lua
@@ -6,3 +6,21 @@ function PLUGIN:OnNPCKilled(npc,attacker,inflictor )
         end
     end
 end
+
+
+function PLUGIN:OnPlayerAreaChanged(client)
+    local character = client:GetCharacter()
+    if(character)then
+        ix.leveling.giveXP(client, 3, true, true)
+    end
+end
+
+
+function  PLUGIN:PlayerDeath(  victim,  inflictor,  attacker )
+    if(attacker:IsPlayer()) then
+        local character = attacker:GetCharacter()
+        if(character)then
+            ix.leveling.giveXP(attacker, 10, false, false)
+        end
+    end
+end
